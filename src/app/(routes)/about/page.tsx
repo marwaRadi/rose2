c2ac61e7527472cb3777cbe9102ea217a6162e92
marwa <meroana201080@gmail.com>
2025-04-14 15:44:18 +0200

    add content

--- src/app/(routes)/about/page.tsx
+++ src/app/(routes)/about/page.tsx
@@ -1,4 +1,16 @@
+import PrimaryBtn from "@/components/common/buttons/PrimaryBtn";
 import { getMetadata } from "../../../_utils/getMetadata";
+import CircleBgIcon from "@/components/common/(circleBgIcon)/CircleBgIcon";
+import { Check } from "lucide-react";
+import Image from "next/image";
+import aboutOne from "@/../public/gifts-background/about/ff4aa9a65b40f281876242848475b272.png";
+import aboutTwo from "@/../public/gifts-background/about/2b8854b043e2c2f1c278829231fcc17b.png";
+import aboutThree from "@/../public/gifts-background/about/33ad3b952fb247e440970b79fbb02f38.png";
+import giftOne from "@/../public/gifts-background/gallery/gift-1.jpg";
+import giftTwo from "@/../public/gifts-background/gallery/gift-2.png";
+import giftThree from "@/../public/gifts-background/gallery/gift-3.png";
+import giftFour from "@/../public/gifts-background/gallery/gift-4.png";
+import giftFive from "@/../public/gifts-background/gallery/gift-5.png";
 
 export const metadata = getMetadata(
   "About",
@@ -6,7 +18,129 @@ export const metadata = getMetadata(
 );
 
 function page() {
-  return <div>about page</div>;
+  return (
+    <main className="container mx-auto 2xl:px-[80px] my-20">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-20 px-3 ">
+        <div className="grid grid-cols-3 grid-rows-2 justify-center  gap-2 ">
+          <div className="self-center col-span-2 row-span-2">
+            <div className="   relative  before:w-[70%] before:h-full  before:absolute before:top-0 before:left-0 before:z-[-1]  before:rounded-[100px] before:rounded-tl-[50px] before:border-2 before:border-primary-dark  aspect-square before:transform before:rotate-4 ">
+              <Image
+                fill
+                className="object-cover py-4 pl-6  rounded-[100px] !rounded-tl-[50px] "
+                src={aboutOne}
+                alt=""
+              />
+            </div>
+          </div>
+
+          <div className=" relative rounded-full self-end  overflow-hidden aspect-square ">
+            <Image fill className="object-cover " src={aboutTwo} alt="" />
+          </div>
+          <div className=" relative  overflow-hidden   rounded-[40px] rounded-r-[50%]  aspect-square">
+            <Image fill className="object-cover " src={aboutThree} alt="" />
+          </div>
+        </div>
+        {/* content */}
+        <div className="capitalize  flex flex-col gap-6">
+          <h4 className=" text-primary-dark track  tracking-[4px] font-bold text-lg uppercase">
+            About Us
+          </h4>
+          <h2 className="font-bold text-xl lg:text-3xl ">
+            We Provide Best And Quality{" "}
+            <span className="text-primary-dark">Gifts Box</span> Product For You
+          </h2>
+          <p>
+            Recusandae tempora aut laborum molestias veniam. A commodi sequi
+            accusantium ullam cupiditate. Neque quidem qui et autem dolor dicta
+            necessitatibus ut ad.
+          </p>
+          <PrimaryBtn>Discover More </PrimaryBtn>
+
+          <ul className="grid grid-cols-1 md:grid-cols-2 gap-3">
+            <li className="flex items-center gap-2">
+              <CircleBgIcon padding="p-1" bg="bg-secondary-dark">
+                <Check />
+              </CircleBgIcon>
+              <span>Streamlined Shipping Experience</span>
+            </li>
+            <li className="flex items-center gap-2">
+              <CircleBgIcon padding="p-1" bg="bg-secondary-dark">
+                <Check />
+              </CircleBgIcon>
+              <span>Affordable Modern Design</span>
+            </li>
+            <li className="flex items-center gap-2">
+              <CircleBgIcon padding="p-1" bg="bg-secondary-dark">
+                <Check />
+              </CircleBgIcon>
+              <span>Competitive Price & Easy To Shop</span>
+            </li>
+            <li className="flex items-center gap-2">
+              <CircleBgIcon padding="p-1" bg="bg-secondary-dark">
+                <Check />
+              </CircleBgIcon>
+              <span>We Made Awesome Products</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      {/* gallery */}
+      <div className=" my-20 px-4">
+        {/* title */}
+        <div className="w-fit md:text-center  md:mx-auto mb-4">
+          <h4 className="text-primary-dark track  tracking-[4px] font-bold  uppercase">
+            Our Gallery
+          </h4>
+          <h2 className="relative  font-bold text-xl pb-1 before:absolute before:w-[90%] before:h-[40%] before:z-[-1] before:bottom-0  before:bg-primary-light-90 before:rounded-r-full after:absolute after:bottom-0 after:w-[45%] after:h-0.5 after:bg-primary-dark after:left-0  ">
+            Let&apos;s Check Our Photo Gallery
+          </h2>
+        </div>
+        {/* images */}
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+          <div className=" relative aspect-square md:order-2">
+            <Image
+              fill
+              src={giftOne}
+              alt=""
+              className="rounded-[40px] object-cover"
+            />
+          </div>
+          <div className=" relative aspect-square md:order-1">
+            <Image
+              fill
+              src={giftTwo}
+              alt=""
+              className="rounded-[40px] object-cover"
+            />
+          </div>
+          <div className=" relative aspect-square md:order-first">
+            <Image
+              fill
+              src={giftThree}
+              alt=""
+              className="rounded-[40px] object-cover"
+            />
+          </div>
+          <div className=" relative aspect-square md:order-last">
+            <Image
+              fill
+              src={giftFour}
+              alt=""
+              className="rounded-[40px] object-cover"
+            />
+          </div>
+          <div className=" relative aspect-square md:order-3 col-span-2 md:aspect-auto">
+            <Image
+              fill
+              src={giftFive}
+              alt=""
+              className="rounded-[40px] object-cover"
+            />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default page;
--- src/app/(routes)/contact/page.tsx
+++ src/app/(routes)/contact/page.tsx
@@ -7,10 +7,158 @@ export const metadata = getMetadata(
 
 function page() {
   return (
-    <div>
-      contact page
-    </div>
-  )
+    <main className="container mx-auto 2xl:px-[80px] my-20 px-[16px]">
+      <h2 className=" uppercase tracking-[4px] mb-[48px] text-primary-dark font-bold">
+        contact us
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2  gap-10">
+        {/* left side */}
+        <div className=" space-y-6 rounded-[20px] shadow-[0px_0px_17px_-5px_rgba(219,61,128,0.2)]  md:w-fit p-6">
+          {/* content */}
+          <div className="flex gap-2">
+            <div className="border w-[70px] h-[70px] flex justify-center items-center rounded-[10px] border-primary-dark text-primary-dark ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-9"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0 6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z"
+                />
+              </svg>
+            </div>
+            <div>
+              <p className="font-bold !text-primary-dark text-xl">
+                Call Anytime
+              </p>
+              <p className="!text-heading">241-373-2123</p>
+            </div>
+          </div>
+          <div className="flex gap-2 ">
+            <div className="border w-[70px] h-[70px] flex justify-center items-center rounded-[10px] border-primary-dark text-primary-dark ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-9"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+                />
+              </svg>
+            </div>
+            <div>
+              <p className="font-bold !text-primary-dark text-xl">Send Email</p>
+              <p className="!text-heading">Dwight63@gmail.com</p>
+            </div>
+          </div>
+          <div className="flex gap-2 ">
+            <div className="border w-[70px] h-[70px] flex justify-center items-center rounded-[10px] border-primary-dark text-primary-dark ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-9"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                />
+              </svg>
+            </div>
+            <div>
+              <p className="font-bold !text-primary-dark text-xl">Visit Us </p>
+              <p className="!text-heading">
+                20 Island Park Road,
+                <br /> New Jearsy, New York, USA
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* Right side */}
+        <form className=" space-y-6">
+          <div className="w-full h-12 relative flex rounded-xl capitalize ">
+            <input
+              itemID="userName"
+              className="peer w-full  outline-none px-4 text-base rounded-xl  border border-primary-dark focus:shadow-md focus:shadow-primary-dark/10"
+              id="userName"
+              type="text"
+              required
+            />
+            <label
+              className="absolute top-1/2 translate-y-[-50%] bg-white left-4 px-2 rounded-full peer-focus:top-0 peer-focus:left-3 font-light 
+              peer-focus:text-sm peer-focus:text-primary-dark 
+              peer-valid:-top-0 peer-valid:left-3 peer-valid:text-sm   peer-valid:text-primary-dark  duration-150"
+              htmlFor="userName"
+            >
+              name
+            </label>
+          </div>
+          <div className="w-full h-12 relative flex rounded-xl capitalize">
+            <input
+              itemID="email"
+              className="peer w-full  outline-none px-4 text-base rounded-xl  border border-primary-dark focus:shadow-md focus:shadow-primary-dark/10"
+              id="email"
+              type="text"
+              required
+            />
+            <label
+              className="absolute top-1/2 translate-y-[-50%] bg-white left-4 px-2 peer-focus:top-0 peer-focus:left-3 font-light peer-focus:text-sm peer-focus:text-primary-dark  peer-valid:text-primary-dark  peer-valid:-top-0 peer-valid:left-3 peer-valid:text-sm   duration-150"
+              htmlFor="email"
+            >
+              email
+            </label>
+          </div>
+          <div className="w-full h-12 relative flex rounded-xl capitalize">
+            <input
+              itemID="phone"
+              className="peer w-full  outline-none px-4 text-base rounded-xl  border border-primary-dark focus:shadow-md focus:shadow-primary-dark/10"
+              id="phone"
+              type="number"
+              required
+            />
+            <label
+              className="absolute top-1/2 translate-y-[-50%] bg-white left-4 px-2 peer-focus:top-0 peer-focus:left-3 font-light peer-focus:text-sm peer-focus:text-primary-dark  peer-valid:text-primary-dark  peer-valid:-top-0 peer-valid:left-3 peer-valid:text-sm   duration-150"
+              htmlFor="phone"
+            >
+              phone
+            </label>
+          </div>
+          <div className="w-full  relative flex rounded-xl capitalize">
+            <textarea
+              itemID="message"
+              className="peer w-full h-[150px] pt-[10px]  outline-none px-4 text-base rounded-xl  border border-primary-dark focus:shadow-md focus:shadow-primary-dark/10"
+              id="message"
+              required
+            />
+            <label
+              className="absolute top-[10%] bg-white left-4 px-2 peer-focus:top-[-10px]  peer-focus:left-3 font-light peer-focus:text-sm peer-focus:text-primary-dark  peer-valid:text-primary-dark  peer-valid:-top-0 peer-valid:left-3 peer-valid:text-sm   duration-150"
+              htmlFor="message"
+            >
+              your message
+            </label>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
 }
 
 export default page
