4037f214fb9058d070d4c17e437cd87457b2ef23
marwa <meroana201080@gmail.com>
2025-05-12 00:56:28 +0300

    fix issue

--- src/components/layout/header/components/CartBadge.tsx
+++ src/components/layout/header/components/CartBadge.tsx
@@ -24,6 +24,7 @@ function CartBadge() {
         if (!res.ok) throw new Error(res.statusText);
         const payload = await res.json();
         getCurrentCartProductsCallback(payload.numOfCartItems);
+        console.log(payload.numOfCartItems);
       } catch (e) {
         if (e instanceof Error) throw new Error(e.message);
       }
@@ -34,7 +35,7 @@ function CartBadge() {
   return (
     <Link href="/cart">
       <div className={` relative `}>
-        {cartCount > 0 && (
+        {cartCount && cartCount > 0 && (
           <div className="absolute -top-3 -right-3  bg-primary-dark w-5 h-5 rounded-full flex justify-center items-center ">
             <p className="text-white">{cartCount}</p>
           </div>
